b5885ccd78f555f67c281610c346c0f5940604f9
ahmed faisal95 <ahmed.fiesal4414@gamil.com>
2025-02-23 18:45:27 +0200

    تعديلات جديدة

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './component/home/Home'
-import About from './component/about/About'
-import Layout from './component/layout/Layout'
-import Blog from './component/blog/Blog'
-import Coffees from './component/coffees/Coffees'
-import Contact from './component/contact/Contact'
-const router=createBrowserRouter([
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./component/home/Home";
+import About from "./component/about/About";
+import Layout from "./component/layout/Layout";
+import Blog from "./component/blog/Blog";
+import Coffees from "./component/coffees/Coffees";
+import Contact from "./component/contact/Contact";
+
+const router = createBrowserRouter([
   {
-path:"coffo/", element : <Layout/> , children  : [
-
-{index:true, element : <Home/>},
-{path : "coffo/about" , element : <About/>},
-{path : "coffo/Blog" , element : <Blog/>},
-{path : "coffo/Coffees" , element : <Coffees/>},
-{path : "coffo/Contact" , element : <Contact/>},
-
-
-]
-
-  }
-])
+    path: "/coffo",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> }, 
+      { path: "about", element: <About /> },
+      { path: "blog", element: <Blog /> },
+      { path: "coffees", element: <Coffees /> },
+      { path: "contact", element: <Contact /> },
+      { path: "Home", element: <Home /> }, 
+    ],
+  },
+]);
 
 export default function App() {
-  return (
-    <RouterProvider router={router}>
-
-    </RouterProvider>
-  )
+  return <RouterProvider router={router} />;
 }
--- src/component/header/Navbar.jsx
+++ src/component/header/Navbar.jsx
@@ -1,97 +1,62 @@
 import React, { useState } from "react";
-import styles from "./styles.module.css";
-import { NavLink, useLocation, useParams } from "react-router-dom";
-import logo from "../../assets/images/logo.png";
+import { NavLink, useLocation } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "../../assets/images/logo.png";
+import styles from "./styles.module.css";
 
-let { navbar, NavLinkes, brandName, mobileMenu, mobileNav ,navbarInHome} = styles;
+let { navbar, NavLinkes, mobileMenu, mobileNav, navbarInHome } = styles;
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
-  const isHome = location.pathname === "/" || location.pathname === "/home";
-  
+  const isHome = location.pathname === "/coffo" || location.pathname === "/coffo/home";
 
   return (
-  <div className="container">
-    <div className="row d-flex justify-content-evenly">
-    <div className={`${ !isHome?navbar:navbarInHome} d-flex justify-content-between align-items-center p-3 `}>
-      {/* Logo */}
-      <div className='ml-auto'>
-        <NavLink to="coffo/" className="text-decoration-none text-light">
-          <img src={logo} alt="logo" />
-        </NavLink>
-      </div>
+    <div className="container">
+      <div className="row d-flex justify-content-evenly">
+        <div className={`${!isHome ? navbar : navbarInHome} d-flex justify-content-between align-items-center p-3`}>
+          
+          {/* Logo */}
+          <div>
+            <NavLink to="/coffo" className="text-decoration-none text-light">
+              <img src={logo} alt="logo" />
+            </NavLink>
+          </div>
 
-      {/* Desktop Menu */}
-      <div className={`${NavLinkes} d-none d-md-block `}>
-        <ul className="d-flex list-unstyled justify-content-around align-items-center ">
-          <li className="px-2 text-center mx-2 bg-danger">
-            <NavLink to="/" className="text-decoration-none text-light">
-              <h6>Home</h6>
-            </NavLink>
-          </li>
-          <li className="px-2 text-center mx-2">
-            <NavLink to="/About" className="text-decoration-none text-light">
-              <h6>About</h6>
-            </NavLink>
-          </li>
-          <li className="px-2 text-center mx-2">
-            <NavLink to="/Coffees" className="text-decoration-none text-light">
-              <h6>Coffees</h6>
-            </NavLink>
-          </li>
-          <li className="px-2 text-center mx-2">
-            <NavLink to="/Blog" className="text-decoration-none text-light">
-              <h6>Blog</h6>
-            </NavLink>
-          </li>
-          <li className="px-2 text-center mx-2">
-            <NavLink to="/contact" className="text-decoration-none text-light">
-              <h6>Contact</h6>
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+          {/* Desktop Menu */}
+          <div className={`${NavLinkes} d-none d-md-block`}>
+            <ul className="d-flex list-unstyled justify-content-around align-items-center">
+              {["Home", "About", "Coffees", "Blog", "Contact"].map((item) => (
+                <li key={item} className="px-2 text-center mx-2">
+                  <NavLink to={`/coffo/${item.toLowerCase()}`} className="text-decoration-none text-light">
+                    <h6>{item}</h6>
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-      {/* Hamburger Menu Button */}
-      <button className="d-md-none text-white bg-transparent border-0" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
-      </button>
+          {/* Hamburger Menu Button */}
+          <button className="d-md-none text-white bg-transparent border-0" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
+          </button>
 
-      {/* Mobile Menu */}
-      <div className={`${mobileNav} ${isOpen ? mobileMenu : "d-none"}`}>
-        <ul className="list-unstyled text-center">
-          <li className="py-2">
-            <NavLink to="/home" className="text-decoration-none text-light">
-              Home
-            </NavLink>
-          </li>
-          <li className="py-2">
-            <NavLink to="/About" className="text-decoration-none text-light">
-              About
-            </NavLink>
-          </li>
-          <li className="py-2">
-            <NavLink to="/Coffees" className="text-decoration-none text-light">
-              Coffees
-            </NavLink>
-          </li>
-          <li className="py-2">
-            <NavLink to="/Blog" className="text-decoration-none text-light">
-              Blog
-            </NavLink>
-          </li>
-          <li className="py-2">
-            <NavLink to="/contact" className="text-decoration-none text-light">
-              Contact
-            </NavLink>
-          </li>
-        </ul>
+          {/* Mobile Menu */}
+          {isOpen && (
+            <div className={`${mobileNav} ${mobileMenu}`}>
+              <ul className="list-unstyled text-center">
+                {["Home", "About", "Coffees", "Blog", "Contact"].map((item) => (
+                  <li key={item} className="py-2">
+                    <NavLink to={`/coffo/${item.toLowerCase()}`} className="text-decoration-none text-light" onClick={() => setIsOpen(false)}>
+                      {item}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </div>
-    </div>
-      
-  </div>
   );
 }
